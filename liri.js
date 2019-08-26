e404e142714e1032576099be8af43a06feee2f60
T3k5upp0rt <kylan.childers@gmail.com>
2019-08-26 13:07:29 -0700

    Created spotify function and created dowhatitsays function to pull from random.txt

--- liri.js
+++ liri.js
@@ -1,7 +1,13 @@
 const axios = require('axios');
+var fs = require("fs");
 require("dotenv").config();
 var keys = require("./keys.js");
-// var spotify = new Spotify(keys.spotify);
+//var spotify = new Spotify(keys.spotify);
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify({
+  id: "76feb939624d4a0fbddca0f8d2292d73",
+  secret: "c4191599c2d547229bd8d9396ba15187"
+});
 
 var command = process.argv[2];
 var input = process.argv[3];
@@ -44,9 +50,55 @@ function movieThis(){
     )
 }
 
+function spotifyThis(){
+    if(input == null){
+        input = "The Sign Ace of Base"
+    };
+    spotify.search({ type: 'track', query: input, limit: 1 }, function(err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
+       
+      console.log("Artist: " + data.tracks.items[0].album.artists[0].name); 
+      console.log("Song: " + data.tracks.items[0].name);
+      console.log("Preview Link: " + data.tracks.items[0].preview_url);
+      console.log("Album Name: " + data.tracks.items[0].album.name);
+      });
+}
+
+function doWhatItSays(){
+    fs.readFile("random.txt", "utf8", function(error, data) {
+
+        if (error) {
+          return console.log(error);
+        }
+      
+        var dataArr = data.split(",");
+
+        var command = dataArr[0];
+
+        if(command === "concert-this"){
+            input = dataArr[1];
+            concertThis()
+        }else if(command === "movie-this"){
+            input = dataArr[1];
+            movieThis()
+        }else if(command === "spotify-this-song"){
+            input = dataArr[1];
+            spotifyThis()
+        }
+
+      
+      });
+}
+
 if(command === "concert-this"){
     concertThis()
 }else if(command === "movie-this"){
     movieThis()
+}else if(command === "spotify-this-song"){
+    spotifyThis()
+}else if(command === "do-what-it-says"){
+    doWhatItSays()
 }
 
